d72dbb93de1302b9e37d149a3d5fbd7ec21ffd32
kubitre <kubitre@gmail.com>
2019-05-23 11:11:42 +0700

    upd

--- entry.go
+++ entry.go
@@ -1,10 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	nameService = "[NUCLEOUS]"
 )
+
+func parser(object *interface{}, pathname string) *interface{} {
+	return nil
+}
+
+func init() {
+	// database := configuration.DatabaseConfiguration{}
+	// localServer := configuration.InitPackage{}
+	// jwtChecker := configuration.JWTPay{}
+}
 
 func main() {
 	fmt.Println(nameService + " was started")
