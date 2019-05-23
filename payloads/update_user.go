4b1306d727e01eed09f44599e680f5e7371a09c9
kubitre <kubitre@gmail.com>
2019-05-23 10:28:09 +0700

    upd

--- payloads/update_user.go
+++ payloads/update_user.go
@@ -1,5 +1,14 @@
 package payloads
 
+import (
+	"errors"
+	"regexp"
+)
+
+const (
+	ReservedTokenNil = "<nil>"
+)
+
 /*UserUpdatePayload - */
 type UserUpdatePayload struct {
 	UserName string `json:"username"`
@@ -7,18 +16,49 @@ type UserUpdatePayload struct {
 	Email    string `json:"email"`
 }
 
-func (pay *UserUpdatePayload) Validate() error {
-	return nil
+const (
+	usernamePattern = "([A-Za-z_0-9])\\w{4,}"
+	passwordPattern = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
+	emailPattern    = "^([A-Z|a-z|0-9](\\.|_){0,1})+[A-Z|a-z|0-9]\\@([A-Z|a-z|0-9])+((\\.){0,1}[A-Z|a-z|0-9]){2}\\.[a-z]{2,3}$"
+)
+
+/*Validate - валидация полей*/
+func (pay *UserUpdatePayload) Validate() *UserUpdatePayload {
+	if err := pay.validateUserName(); err != nil {
+		pay.UserName = ReservedTokenNil
+	}
+	if err2 := pay.validatePassword(); err2 != nil {
+		pay.Password = ReservedTokenNil
+	}
+	if err3 := pay.validateEmail(); err3 != nil {
+		pay.Email = ReservedTokenNil
+	}
+	return pay
 }
 
 func (pay *UserUpdatePayload) validateUserName() error {
-	return nil
+	match, _ := regexp.MatchString(usernamePattern, pay.UserName)
+	if match {
+		return nil
+	} else {
+		return errors.New("unsupporting username")
+	}
 }
 
 func (pay *UserUpdatePayload) validatePassword() error {
-	return nil
+	match, _ := regexp.MatchString(passwordPattern, pay.Password)
+	if match {
+		return nil
+	} else {
+		return errors.New("unsupporting password")
+	}
 }
 
 func (pay *UserUpdatePayload) validateEmail() error {
-	return nil
+	match, _ := regexp.MatchString(emailPattern, pay.Email)
+	if match {
+		return nil
+	} else {
+		return errors.New("unsupporting email")
+	}
 }
