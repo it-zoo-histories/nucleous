c511e6634a10f525a3228d90874cc282cbf73370
kubitre <kubitre@gmail.com>
2019-05-23 11:09:18 +0700

    upd

--- security/ecdsa_gen.go
+++ security/ecdsa_gen.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"nucleous/configuration"
 	"nucleous/dao"
+	"nucleous/payloads"
 
 	"github.com/jenazads/gojwt"
 )
@@ -16,24 +17,37 @@ type ECDSAMiddle struct {
 }
 
 /*Login - авторизация*/
-func (middleware *ECDSAMiddle) Login() error {
+func (middleware *ECDSAMiddle) Login(obj *payloads.LoginPayload) error {
 	fmt.Println("test")
 	return nil
 }
 
 /*Registration - регистрация*/
-func (middleware *ECDSAMiddle) Registration() error {
+func (middleware *ECDSAMiddle) Registration(obj *payloads.RegistrationPayload) error {
 	return nil
 }
 
 /*Logout - выход из аккаунта*/
-func (middleware *ECDSAMiddle) Logout() error {
+func (middleware *ECDSAMiddle) Logout(obj *payloads.LogoutPayload) error {
 	return nil
 }
 
 /*New - создание новой прослойки*/
 func (middleware *ECDSAMiddle) New(tokensDao *dao.TokenDAO, usersDao *dao.UserDAO, config *configuration.JWTPay) *ECDSAMiddle {
-	jwtMiddle, err := gojwt.NewGojwtECDSA(config.NameServer, config.KeyName, config.PrivateKeyPath, config.PublicKeyPath, config.LengthKey, config.AmountHoursExpiredToken)
+	jwtMiddle, _ := gojwt.NewGojwtECDSA(config.NameServer, config.KeyName, config.PrivateKeyPath, config.PublicKeyPath, config.LengthKey, config.AmountHoursExpiredToken)
 	middleware.JWTEcdsa = jwtMiddle
+	middleware.TokenDao = tokensDao
+	middleware.UserDao = usersDao
 	return nil
+}
+
+/*CheckEnterUserToDatabase - проверка пользователя в базе данных*/
+func (middleware *ECDSAMiddle) CheckEnterUserToDatabase(obj payloads.LoginPayload) {
+
+}
+
+/*tokenValidation - проверка токена*/
+func (middleware *ECDSAMiddle) tokenValidation(token string) (bool, error) {
+	result, _, err := middleware.JWTEcdsa.ValidateToken(token)
+	return result, err
 }
